70b271a461770148bf35bf9f3d4de421c06681d5
Ghani M <rhani.data@gmail.com>
2023-12-29 18:34:06 +0100

    ERROR in ./src/utils/dataMocked.js 3:0-44

--- frontend/src/utils/dataMocked.js
+++ frontend/src/utils/dataMocked.js
@@ -1,65 +1,69 @@
-import { mockedData } from "./mock/dataMocked";
+import mockedData from '../mock/dataMocked';
 
 /**
- * Récupère les informations sur l'utilisateur.
- * @param {number} id - Identifiant de l'utilisateur.
- * @returns {object} - Objet contenant les informations de l'utilisateur.
- */
-export const getUserInfosMockedData = async (id) => {
-  try {
-    // Recherche de l'utilisateur dans USER_MAIN_DATA
-    const res = mockedData.USER_MAIN_DATA.find((el) => el.id == id);
-    return { data: res };
-  } catch (e) {
-    // Gestion des erreurs
-    console.log(e);
-  }
-};
+ * Récupère les informations sur l'utilisateur.
+ *
+ * @param {number} id - Identifiant de l'utilisateur.
+ * @returns {object} - Objet contenant les informations de l'utilisateur.
+ */
+export function getUserInfosMockedData(id) {
+  try {
+    // Recherche de l'utilisateur dans USER_MAIN_DATA
+    const user = mockedData.USER_MAIN_DATA.find((el) => el.id === id);
+    return { data: user };
+  } catch (e) {
+    // Gestion des erreurs
+    console.log(e);
+  }
+}
 
 /**
- * Récupère l'activité de l'utilisateur.
- * @param {number} id - Identifiant de l'utilisateur.
- * @returns {object} - Objet contenant les données d'activité de l'utilisateur.
- */
-export const getUserActivityMockedData = async (id) => {
-  try {
-    // Recherche de l'activité de l'utilisateur dans USER_ACTIVITY
-    const res = mockedData.USER_ACTIVITY.find((el) => el.userId == id);
-    return { data: res };
-  } catch (e) {
-    // Gestion des erreurs
-    console.log(e);
-  }
-};
+ * Récupère l'activité de l'utilisateur.
+ *
+ * @param {number} id - Identifiant de l'utilisateur.
+ * @returns {object} - Objet contenant les données d'activité de l'utilisateur.
+ */
+export function getUserActivityMockedData(id) {
+  try {
+    // Recherche de l'activité de l'utilisateur dans USER_ACTIVITY
+    const activity = mockedData.USER_ACTIVITY.find((el) => el.userId === id);
+    return { data: activity };
+  } catch (e) {
+    // Gestion des erreurs
+    console.log(e);
+  }
+}
 
 /**
- * Récupère la durée moyenne des sessions de l'utilisateur.
- * @param {number} id - Identifiant de l'utilisateur.
- * @returns {object} - Objet contenant la durée moyenne des sessions de l'utilisateur.
- */
-export const getUserAverageSessionsMockedData = async (id) => {
-  try {
-    // Recherche des sessions moyennes de l'utilisateur dans USER_AVERAGE_SESSIONS
-    const res = mockedData.USER_AVERAGE_SESSIONS.find((el) => el.userId == id);
-    return { data: res };
-  } catch (e) {
-    // Gestion des erreurs
-    console.log(e);
-  }
-};
+ * Récupère la durée moyenne des sessions de l'utilisateur.
+ *
+ * @param {number} id - Identifiant de l'utilisateur.
+ * @returns {object} - Objet contenant la durée moyenne des sessions de l'utilisateur.
+ */
+export function getUserAverageSessionsMockedData(id) {
+  try {
+    // Recherche des sessions moyennes de l'utilisateur dans USER_AVERAGE_SESSIONS
+    const averageSessions = mockedData.USER_AVERAGE_SESSIONS.find((el) => el.userId === id);
+    return { data: averageSessions };
+  } catch (e) {
+    // Gestion des erreurs
+    console.log(e);
+  }
+}
 
 /**
- * Récupère les performances de l'utilisateur.
- * @param {number} id - Identifiant de l'utilisateur.
- * @returns {object} - Objet contenant les performances de l'utilisateur.
- */
-export const getUserPerformanceMockedData = async (id) => {
-  try {
-    // Recherche des performances de l'utilisateur dans USER_PERFORMANCE
-    const res = mockedData.USER_PERFORMANCE.find((el) => el.userId == id);
-    return { data: res };
-  } catch (e) {
-    // Gestion des erreurs
-    console.log(e);
-  }
-};
+ * Récupère les performances de l'utilisateur.
+ *
+ * @param {number} id - Identifiant de l'utilisateur.
+ * @returns {object} - Objet contenant les performances de l'utilisateur.
+ */
+export function getUserPerformanceMockedData(id) {
+  try {
+    // Recherche des performances de l'utilisateur dans USER_PERFORMANCE
+    const performance = mockedData.USER_PERFORMANCE.find((el) => el.userId === id);
+    return { data: performance };
+  } catch (e) {
+    // Gestion des erreurs
+    console.log(e);
+  }
+}
--- frontend/src/utils/mock/dataMocked.js
+++ frontend/src/utils/mock/dataMocked.js
@@ -1,96 +1,101 @@
-export const USER_MAIN_DATA = [
-    {
-      id: 12,
-      userInfos: {
-        firstName: 'Ghani',
-        lastName: 'Manseur',
-        age: 31,
-      },
-      todayScore: 0.12,
-      keyData: {
-        calorieCount: 1930,
-        proteinCount: 155,
-        carbohydrateCount: 290,
-        lipidCount: 50
-      }
+const mockedData = {};
+
+export default mockedData;
+
+// Données simulées pour USER_MAIN_DATA
+const USER_MAIN_DATA = [
+  {
+    id: 12,
+    userInfos: {
+      firstName: 'Ghani',
+      lastName: 'Manseur',
+      age: 31,
     },
-    {
-      id: 18,
-      userInfos: {
-        firstName: 'Ju',
-        lastName: 'Dav',
-        age: 34,
-      },
-      score: 0.3,
-      keyData: {
-        calorieCount: 2500,
-        proteinCount: 90,
-        carbohydrateCount: 150,
-        lipidCount: 120
-      }
+    todayScore: 0.12,
+    keyData: {
+      calorieCount: 1930,
+      proteinCount: 155,
+      carbohydrateCount: 290,
+      lipidCount: 50
     }
-  ];
-  
-  // Données simulées pour USER_ACTIVITY
-  const USER_ACTIVITY = [
-    {
-      userId: 12,
-      sessions: [
-        // ... données de sessions pour l'utilisateur 12 ...
-      ]
+  },
+  {
+    id: 18,
+    userInfos: {
+      firstName: 'Ju',
+      lastName: 'Dav',
+      age: 34,
     },
-    {
-      userId: 18,
-      sessions: [
-        // ... données de sessions pour l'utilisateur 18 ...
-      ]
+    score: 0.3,
+    keyData: {
+      calorieCount: 2500,
+      proteinCount: 90,
+      carbohydrateCount: 150,
+      lipidCount: 120
     }
-  ];
-  
-  // Données simulées pour USER_AVERAGE_SESSIONS
-  const USER_AVERAGE_SESSIONS = [
-    {
-      userId: 12,
-      sessions: [
-        // ... données de sessions moyennes pour l'utilisateur 12 ...
-      ]
+  }
+];
+
+// Données simulées pour USER_ACTIVITY
+const USER_ACTIVITY = [
+  {
+    userId: 12,
+    sessions: [
+      // ... données de sessions pour l'utilisateur 12 ...
+    ]
+  },
+  {
+    userId: 18,
+    sessions: [
+      // ... données de sessions pour l'utilisateur 18 ...
+    ]
+  }
+];
+
+// Données simulées pour USER_AVERAGE_SESSIONS
+const USER_AVERAGE_SESSIONS = [
+  {
+    userId: 12,
+    sessions: [
+      // ... données de sessions moyennes pour l'utilisateur 12 ...
+    ]
+  },
+  {
+    userId: 18,
+    sessions: [
+      // ... données de sessions moyennes pour l'utilisateur 18 ...
+    ]
+  }
+];
+
+// Données simulées pour USER_PERFORMANCE
+const USER_PERFORMANCE = [
+  {
+    userId: 12,
+    kind: {
+      1: 'cardio',
+      2: 'energy',
+      3: 'endurance',
+      4: 'strength',
+      5: 'speed',
+      6: 'intensity'
     },
-    {
-      userId: 18,
-      sessions: [
-        // ... données de sessions moyennes pour l'utilisateur 18 ...
-      ]
-    }
-  ];
-  
-  // Données simulées pour USER_PERFORMANCE
-  const USER_PERFORMANCE = [
-    {
-      userId: 12,
-      kind: {
-        1: 'cardio',
-        2: 'energy',
-        3: 'endurance',
-        4: 'strength',
-        5: 'speed',
-        6: 'intensity'
-      },
-      data: [
-        // ... données de performance pour l'utilisateur 12 ...
-      ]
+    data: [
+      // ... données de performances pour l'utilisateur 12 ...
+    ]
+  },
+  {
+    userId: 18,
+    kind: {
+      1: 'cardio',
+      2: 'energy',
+      3: 'endurance',
+      4: 'strength',
+      5: 'speed',
+      6: 'intensity'
     },
-    {
-      userId: 18,
-      kind: {
-        1: 'cardio',
-        2: 'energy',
-        3: 'endurance',
-        4: 'strength',
-        5: 'speed',
-        6: 'intensity'
-      },
-      data: [
-        // ... données de performance pour l'utilisateur 18 ...
-      ]
-    }
-  ];
+    data: [
+      // ... données de performances pour l'utilisateur 18 ...
+    ]
+  }
+];
